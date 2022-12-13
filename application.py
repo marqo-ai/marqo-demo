a1f908a989980d867e4fd1fe3e6bc9c9e592bb73
im-vivien <vivien@marqo.ai>
2022-12-13 16:44:54 +0800

    rename app to application

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__, static_folder="build", static_url_path="/")
+application = Flask(__name__, static_folder="build", static_url_path="/")
 # api = Api(app)
 
 
@@ -19,7 +19,7 @@ app = Flask(__name__, static_folder="build", static_url_path="/")
 #     return app.send_static_file("index.html")
 
 
-@app.route("/")
+@application.route("/")
 def home():
     return "<h1 style='color:blue'>Welcome to Marqo!</h1>"
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from application import app
+from application import application as app
 
 
 if __name__ == "__main__":
