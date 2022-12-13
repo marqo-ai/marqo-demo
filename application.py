c364c31ac72f0cba4d1adba3fcc63a9f0fa39943
im-vivien <vivien@marqo.ai>
2022-12-13 20:02:32 +0800

    Add build to buildspec

--- application.py
+++ application.py
@@ -7,21 +7,15 @@ from api.index import HealthResource
 
 application = Flask(__name__, static_folder="build", static_url_path="/")
 api = Api(application)
+parser = reqparse.RequestParser()
+parser.add_argument("task")
 
-
-@application.route("/api/health", methods=["GET"])
-def health():
-    return "<h1 style='color:blue'>Welcome to Marqo!</h1>"
+# APIs
+api.add_resource(HealthResource, "/api/health")
+api.add_resource(CoreAPIResource, "/api/core")
 
 
 @application.route("/")
 def ui():
+    # static
     return application.send_static_file("index.html")
-
-
-parser = reqparse.RequestParser()
-parser.add_argument("task")
-
-
-api.add_resource(HealthResource, "/api/health")
-api.add_resource(CoreAPIResource, "/api/core")
