d1a5b1d5d7a5deac295c33e48877294d5d00cc7e
Tom <tomhamer@live.com>
2022-12-30 10:46:20 +1100

    Update Upload-Img.tsx

--- ui/src/components/Search-Hero/Upload-Img.tsx
+++ ui/src/components/Search-Hero/Upload-Img.tsx
@@ -18,16 +18,6 @@ export const UploadImg = () => {
 
     return <Fragment>
         <div className="contents">
-            <div className="flex items-center justify-center w-full">
-                <label htmlFor="img-file" className="flex flex-col items-center justify-center w-[fit-content] cursor-pointer">
-                    <div className="flex flex-col items-center justify-center">
-                        <p className={`${theme === "dark" ? "text-white hover:text-secondary border-slate-200 hover:border-secondary" : "text-primary hover:text-primary border-primary"} hover:scale-[0.97] text-xs md:text-sm px-2 py-[6px] md:p-0 border-[1px] md:border-none rounded-md md:rounded-none md:underline whitespace-nowrap`}>
-                            or upload an image
-                        </p>
-                    </div>
-                    <Input onInput={(e) => handleOnChange(e)} type={"file"} id="img-file" className={`hidden`} ref={inputRef} />
-                </label>
-            </div>
         </div>
     </Fragment>
 }
