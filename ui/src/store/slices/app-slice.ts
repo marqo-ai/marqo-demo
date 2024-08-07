8bc693b54eaee575a862df3e7cf91fd7afb6f13b
Tom <tomhamer@live.com>
2024-08-07 11:13:49 -0700

    Update app-slice.ts

--- ui/src/store/slices/app-slice.ts
+++ ui/src/store/slices/app-slice.ts
@@ -3,7 +3,7 @@ import { AdvancedSettings, SearchHit, SearchResponse, SearchSettings } from '../
 import { SIMPLEWIKI, ECOMMERCE, DIVERSEIMAGES } from '../../commons/constants';
 
 // export const DEFAULT_Q = 'an alleyway in a futuristic cyberpunk city';
-export const DEFAULT_Q = 'a blue handbag with a gold clasp';
+export const DEFAULT_Q = 'when was isaac newton born';
 
 export type DatasetTypes = typeof SIMPLEWIKI | typeof ECOMMERCE | typeof DIVERSEIMAGES;
 
